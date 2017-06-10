c542eee68360ad355d7e03ba1a7166781c291add
Bozhin Zafirov <bozhin@sirius.local>
2017-06-10 16:23:43 +0300

    optimize and add error handling

--- brightness.rs
+++ brightness.rs
@@ -1,55 +1,56 @@
-use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
 /* read_brightness reads integer value from a text file */
-fn read_brightness(name: &str) -> i32 {
+fn read_brightness(name: &str) -> Result<i32, std::io::Error> {
 	// open file for reading
 	let mut f = match File::open(name) {
-		Err(why) => panic!("File::open({}): {}", name, why.description()),
+		Err(e) => return Err(e),
 		Ok(file) => file,
 	};
 	// read file contents
 	let mut contents = String::new();
 	match f.read_to_string(&mut contents) {
-		Err(why) => panic!("read_to_string: {}", why.description()),
+		Err(e) => return Err(e),
 		Ok(_) => (),
 	};
 	// convert to int and return
 	let brightness:i32 = contents.trim_matches('\n').parse().unwrap();
-	brightness
+	Ok(brightness)
 }
 
 /* adjust_brightness updates a brightness value into a file */
-fn adjust_brightness(name: &str, delta: i32, max: i32) {
-	let mut brightness = read_brightness(name);
+fn adjust_brightness(name: &str, delta: i32, max: i32) -> Result<i32, std::io::Error> {
 	// calculate next brightness value
-	brightness += delta;
-	if brightness > max {
-		brightness = max;
-	}
-	if brightness < 0 {
-		brightness = 0;
-	}
+	let mut brightness = match read_brightness(name) {
+		Err(e) => return Err(e),
+		Ok(b) => b + delta,
+	};
+	brightness = std::cmp::min(brightness, max);
+	brightness = std::cmp::max(brightness, 0);
 	// save new value to file
 	let mut f = match File::create(name) {
-		Err(why) => panic!("could not open {}: {}", name, why.description()),
+		Err(e) => return Err(e),
 		Ok(file) => file,
 	};
 	match f.write_all(brightness.to_string().as_bytes()) {
-		Err(why) => panic!("could not write to file: {}", why.description()),
-		Ok(_) => (),
+		Err(e) => return Err(e),
+		Ok(_) => (brightness),
 	};
+	Ok(brightness)
 }
 
 /* main program */
 fn main() {
-	let args:Vec<String> = env::args().collect();
+	let args:Vec<String> = std::env::args().collect();
 	let srcfile = &args[1];
 	let max:i32 = (&args[2]).parse().unwrap();
 	let delta:i32 = (&args[3]).parse().unwrap();
 	if delta != 0 {
-		adjust_brightness(srcfile, delta, max);
+		match adjust_brightness(srcfile, delta, max) {
+			Err(why) => println!("adjust_brightness: {}", why.description()),
+			Ok(brightness) => println!("new brightness: {}", brightness),
+		};
 	}
 }
